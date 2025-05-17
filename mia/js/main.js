3c2edeb28056d685663084584900b7d9e85e74db
plunyo <conradthieu87@gmail.com>
2025-05-17 23:14:12 +1000

    a

---
+++ mia/js/main.js
@@ -0,0 +1,21 @@
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = "to mia, from conrad – with code & care".split("");
+    const titleElement = document.getElementById("title");
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 100); // 1000ms delay
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
